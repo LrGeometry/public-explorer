8eda414395517ce2a5df1662a0111080cff72843
linux08 <abimbola130@gmail.com>
2019-06-14 21:45:46 +0100

    more changes

---
+++ src/components/Card.js
@@ -0,0 +1,76 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { animated, interpolate } from "react-spring/hooks";
+import Carousel from "nuka-carousel";
+
+const InnerComp = () => (
+  <div className="mt-3 border rounded-lg" >
+    <div style={{ backgroundColor: "#e2e6f9" }}>
+      <div className="pl-3 pt-2">
+        <p style={{ fontSize: 11, color: "#cbccd2" }}> factory entity </p>
+      </div>
+      <div className="d-flex flex-row pl-3 flex1 flex-row">
+        <div className="flex flex1 jc">
+          <p style={{ fontSize: 14, color: "#091140" }}> date 33</p>
+        </div>
+
+        <div className="flex flex1 jc">
+          <img style={{ height: "15px" }} src="https://via.placeholder.com/150" alt="testo" />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+class Card extends React.Component {
+  render() {
+    const { i, x, y, rot, scale, trans, bind, data } = this.props;
+    const { name, age, distance, text, pics } = data[i];
+
+    return (
+      <animated.div
+        className="flex flex1"
+        key={i}
+        style={{
+          backgroundColor: "#4fD0e9",
+          transform: interpolate(
+            [x, y],
+            (x, y) => `translate3d(${x}px,${y}px,0)`
+          )
+        }}
+      >
+        <animated.div
+          style={{
+            transform: interpolate([rot, scale], trans)
+          }}
+          className="flex border rounded-lg"
+          {...bind(i)}>
+          <div
+            className="d-flex flex-column  ml-3 mr-3 mt-5 mb-5 flex flex1">
+            <div className="d-flex flex-column flex5">
+
+              <InnerComp />
+              <InnerComp />
+              <InnerComp />
+            </div>
+
+            <div className="d-flex align-items-center flex4">
+              <button style={{ width: "100%" }} type="button" className="btn btn-primary">View factom chain</button>
+            </div>
+
+          </div>
+        </animated.div>
+      </animated.div>
+    );
+  }
+}
+
+Card.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  distance: PropTypes.string,
+  text: PropTypes.string,
+  pics: PropTypes.array
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = Component => {
+  return ReactDOM.render(<App />, document.getElementById('root'));
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
+
 serviceWorker.unregister();
