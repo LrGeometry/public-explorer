61c2e98583ccb3adf1347bbdbbb368d783451699
linux08 <abimbola130@gmail.com>
2019-07-09 23:01:03 +0100

    Moved config to environmental variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,23 @@
 import firebase from "firebase";
+const {
+  REACT_APP_apiKey: apiKey,
+  REACT_APP_authDomain: authDomain,
+  REACT_APP_databaseURL: databaseURL,
+  REACT_APP_projectId: projectId,
+  REACT_APP_storageBucket: storageBucket,
+  REACT_APP_messagingSenderId: messagingSenderId
+} = process.env;
+
+console.log("sjsjd", apiKey);
 const config = {
-  apiKey: "AIzaSyB4c-dlOic0fYfcCUwNbfDtwxj-QDcujOA",
-  authDomain: "hercone-8025f.firebaseapp.com",
-  databaseURL: "https://hercone-8025f.firebaseio.com",
-  projectId: "hercone-8025f",
-  storageBucket: "hercone-8025f.appspot.com",
-  messagingSenderId: "329151475948"
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId
 };
+
 firebase.initializeApp(config);
 export const firebaseDb = firebase.database();
 export default firebase;
