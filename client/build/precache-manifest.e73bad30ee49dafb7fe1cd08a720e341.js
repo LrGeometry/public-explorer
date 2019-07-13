7dbf8c73599425c79131f586e786dc717f34494e
linux08 <abimbola130@gmail.com>
2019-07-13 13:30:01 +0100

    More deployment fixes

---
+++ client/build/precache-manifest.e73bad30ee49dafb7fe1cd08a720e341.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a2b49dcf462175e1ed87145f7366c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de3aba32fb47d45942a6",
+    "url": "/static/css/main.817efd83.chunk.css"
+  },
+  {
+    "revision": "5a0a69c7f0fa632c2d60",
+    "url": "/static/js/2.6d86b291.chunk.js"
+  },
+  {
+    "revision": "de3aba32fb47d45942a6",
+    "url": "/static/js/main.36570df2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "32bf719089f7caf77c0ea2e90d850771",
+    "url": "/static/media/camera-solid.32bf7190.svg"
+  },
+  {
+    "revision": "54779019087c96413f3df9a334eb9674",
+    "url": "/static/media/circle-solid.54779019.svg"
+  },
+  {
+    "revision": "e4fce4899fc6c18f15f4cc6bcf52c7a5",
+    "url": "/static/media/copy-regular.e4fce489.svg"
+  },
+  {
+    "revision": "cf5e4039d88b7054bd1ac282e861a537",
+    "url": "/static/media/file-alt-solid.cf5e4039.svg"
+  },
+  {
+    "revision": "dab24c4b3866c9475322d26a191699ea",
+    "url": "/static/media/herc2.dab24c4b.png"
+  },
+  {
+    "revision": "d9a24d05eed3db4d122adcaf0ac3d956",
+    "url": "/static/media/info-circle-solid.d9a24d05.svg"
+  },
+  {
+    "revision": "bcd851cd39e3e034a635318b14cbc85b",
+    "url": "/static/media/photos.bcd851cd.svg"
+  }
+]);
